7950c1b287aafd7fab85c1a7d85627ecbfebda8d
Manoj <manojcse1999@gmail.com>
2022-07-15 23:01:12 +0530

    removed all files

--- src/main/java/com/company/restAPI/hotel/HotelRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.restAPI.hotel;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface HotelRepository extends CrudRepository<Hotel,Integer>{
-
-}
--- src/main/java/com/company/restAPI/user/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.company.restAPI.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-	User findByUsername(String username);
-}
